61e56f6a71564a2b4045728743ca75592e6a0bae
Aidan Stritch <aidan.stritch@gmail.com>
2019-05-05 19:44:50 +0000

    added defensive programming

--- spec/calcSpec.js
+++ spec/calcSpec.js
@@ -3,5 +3,11 @@ describe("Calculator", function(){
         it("should return 42", function() {
             expect(addition(20,22)).toBe(42);
         });
+        it("should return 26", function() {
+            expect(addition(7,19)).toBe(26);
+        });
+        it("should supply an error if we don't supply two numbers", function() {
+            expect(addition("Hitchikers","Guide")).toBe("Error!");
+        });
     });
 });
